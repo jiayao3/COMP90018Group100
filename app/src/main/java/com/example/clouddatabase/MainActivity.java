023b6f6d68943a95e85411792170ee15f05b4b17
Sean <liseanw@student.unimelb.edu.au>
2023-09-23 07:58:40 +1000

    Created a simple login page

--- app/src/main/java/com/example/clouddatabase/MainActivity.java
+++ app/src/main/java/com/example/clouddatabase/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.clouddatabase;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+// TO DO before running properly
+// 1. Create a Firebase account and establish a project (https://firebase.google.com)
+// 2. Links the project with this application (follow the steps of Firebase)
+// 3. Create a real time database in the Firebase project
+
+//Tips:
+// 1. Add the use of Firebase real-time database in your gradle settings: https://firebase.google.com/docs/database/android/start
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+
+        initialFireBase("Test");
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String outputText = "Testing";
+                myRef.setValue(outputText);
+            }
+        });
     }
+
+    private void initialFireBase(String referenceName){
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = firebaseDatabase.getReference(referenceName);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String inputText = snapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
 }
