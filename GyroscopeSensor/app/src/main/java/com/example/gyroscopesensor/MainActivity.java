af699a29fb0cfea881d719e77ef918e9d8afec64
lamooiety <clairesss.no1@gmail.com>
2023-09-29 16:01:30 +1000

    detect phone position
    background changed based the phone position:
    
    give me a function to change set the background color blue If the phone is tilted at an angle where the left side is low and the right side is high , and yellow If the phone is tilted at an angle where the right side is low and the left side is high, set to white background is the phone is at its initial

--- GyroscopeSensor/app/src/main/java/com/example/gyroscopesensor/MainActivity.java
+++ GyroscopeSensor/app/src/main/java/com/example/gyroscopesensor/MainActivity.java
@@ -2,48 +2,71 @@ package com.example.gyroscopesensor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private SensorManager sensorManager;
-    private Sensor gyroscopeSensor;
-    private SensorEventListener gyroscopeEventListener;
+    private Sensor accelerometerSensor;
+    private SensorEventListener accelerometerListener;
+    private boolean isInitial = true; // Flag to track the initial position
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        // Initialize sensors and listeners
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-        if (gyroscopeSensor == null) {
-            Toast.makeText(this, "This device has no Gyroscope!", Toast.LENGTH_SHORT).show();
-            finish();
-        }
-
-        gyroscopeEventListener = new SensorEventListener() {
+        // Create a sensor event listener
+        accelerometerListener = new SensorEventListener() {
             @Override
-            public void onSensorChanged(SensorEvent sensorEvent) {
-                // clockwise
-                if (sensorEvent.values[2] > 0.5f) {
+            public void onSensorChanged(SensorEvent event) {
+                float xAcceleration = event.values[0];
+                float yAcceleration = event.values[1];
+                float zAcceleration = event.values[2];
+
+                // Calculate the total acceleration (magnitude)
+                double totalAcceleration = Math.sqrt(
+                        xAcceleration * xAcceleration +
+                                yAcceleration * yAcceleration +
+                                zAcceleration * zAcceleration
+                );
+
+                // Calculate the angle of tilt in degrees
+                double tiltAngleDegrees = Math.toDegrees(Math.asin(xAcceleration / totalAcceleration));
+
+                // Define thresholds for left and right tilt angles (adjust as needed)
+                double leftTiltThreshold = -15.0; // Example threshold for left tilt
+                double rightTiltThreshold = 15.0; // Example threshold for right tilt
+
+                // Check if the phone is tilted to the left
+                if (tiltAngleDegrees < leftTiltThreshold) {
                     getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-                // anti-clockwise
-                } else if (sensorEvent.values[2] < 0.5f) {
+                }
+                // Check if the phone is tilted to the right
+                else if (tiltAngleDegrees > rightTiltThreshold) {
                     getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                }
+                // If the phone is near the initial position, set the background to white
+                else {
+                    if (!isInitial) {
+                        getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+                    }
                 }
             }
 
             @Override
-            public void onAccuracyChanged(Sensor sensor, int i) {
-
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                // Handle accuracy changes if needed
             }
         };
     }
@@ -51,12 +74,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(gyroscopeEventListener, gyroscopeSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        sensorManager.registerListener(accelerometerListener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        sensorManager.unregisterListener(gyroscopeEventListener);
+        sensorManager.unregisterListener(accelerometerListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Ensure that the flag is set to true when the activity is destroyed
+        isInitial = true;
     }
 }
