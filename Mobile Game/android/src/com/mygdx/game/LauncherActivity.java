e29c3990258ac87421af08bd225194103b685284
jiayao3 <wujiayao.jiayao@gmail.com>
2023-10-25 15:03:20 +1100

    empty view activity added

---
+++ Mobile Game/android/src/com/mygdx/game/LauncherActivity.java
@@ -0,0 +1,35 @@
+
+package com.mygdx.game;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
+        setContentView(R.layout.activity_main);
+
+        Button startGameButton = findViewById(R.id.startGameButton);
+        startGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGame();
+            }
+        });
+    }
+
+    private void startGame() {
+        // Launch your libGDX game activity
+        Intent intent = new Intent(this, AndroidLauncher.class);
+        startActivity(intent);
+
+        // Finish the current activity
+        finish();
+    }
+}
