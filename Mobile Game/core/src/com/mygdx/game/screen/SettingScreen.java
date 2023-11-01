5af5ba8d8594032a5e94026071a48bc45b5bd97c
lamooiety <clairesss.no1@gmail.com>
2023-11-01 19:41:15 +1100

    added codes for trying different fonts

--- Mobile Game/core/src/com/mygdx/game/screen/SettingScreen.java
+++ Mobile Game/core/src/com/mygdx/game/screen/SettingScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -43,7 +44,19 @@ public class SettingScreen implements Screen {
         // Create UI widgets
         Table table = new Table();
         table.setFillParent(true);
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("Karma Future.otf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter params = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        params.size = 14; // Set font size
+        BitmapFont font = generator.generateFont(params);
+        generator.dispose();
+
+
+
+
         Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+        skin.add("otf-font", font, BitmapFont.class);
+
         Label.LabelStyle labelStyle = new Label.LabelStyle(skin.getFont("default-font"), skin.getColor("white"));
 
         Label label = new Label("Sample Text", labelStyle);
