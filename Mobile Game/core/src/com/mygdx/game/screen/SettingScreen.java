f1f34846ed31e10ef45a015bd70010ffba3e6422
lamooiety <clairesss.no1@gmail.com>
2023-11-02 02:03:12 +1100

    Option Screen Style Updated
    1. figured out the way of importing customised font to uiskin
        - the font size is not flexible, if you want a different size but same font style, you need to import a new one from start;
    
    2. TextButton.TextButtonStyle can set the font color depending on the button mode (up, down, checked etc...)
    
    3. The buttons are set to be logically checked or non-checked
    
    4. However, notice that the button color (not font color) is changed by hard code (becuase uiskin does not allow us to change the button color)
    
    4. The Back button is made by programming instead of a plain image
    
    Large Progress: Make all buttons reactable whenever chosen or clicking, including the back button.

--- Mobile Game/core/src/com/mygdx/game/screen/SettingScreen.java
+++ Mobile Game/core/src/com/mygdx/game/screen/SettingScreen.java
@@ -2,10 +2,17 @@ package com.mygdx.game.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.FileHandleResolver;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGeneratorLoader;
+import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -14,6 +21,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mygdx.game.AttackMode;
@@ -21,6 +30,10 @@ import com.mygdx.game.ControlMode;
 import com.mygdx.game.Game;
 
 public class SettingScreen implements Screen {
+    private final String NOT_SELECTED_BUTTON_COLOR = "#255f85";
+    private final String SELECTED_BUTTON_COLOR = "#ff6f61";
+    final float BACK_BUTTON_WIDTH = Gdx.graphics.getWidth() * 0.45f;
+    final float BACK_BUTTON_HEIGHT = Gdx.graphics.getHeight() * 0.07f;
     private Stage stage;
     private Game game;
     private Label descriptionLabel;
@@ -40,25 +53,59 @@ public class SettingScreen implements Screen {
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
+        // Create a skin
+        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+
+        // Add Font for title-labels in skin
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("font/Karma Future.otf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 80; // set font size
+        // parameter.color = Color.valueOf("#ffffff"); // set font color
+        BitmapFont titleLabelFont = generator.generateFont(parameter);
+        generator.dispose();
+        skin.add("titleLabelFont", titleLabelFont);
+
+        // Add Font for description labels in skin
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("font/light_pixel-7.ttf"));
+        parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 40; // set font size
+        // parameter.color = Color.valueOf("#dfec11"); // set font color
+        BitmapFont descLabelFont = generator.generateFont(parameter);
+        generator.dispose();
+        skin.add("descLabelFont", descLabelFont);
+
+        // Set the style (with color) for title and description labels
+        Label.LabelStyle titleLabelStyle = new Label.LabelStyle(skin.getFont("titleLabelFont"), Color.valueOf("#ffffff"));
+        Label.LabelStyle descLabelStyle = new Label.LabelStyle(skin.getFont("descLabelFont"), Color.valueOf("#e8e288"));
+
+
+        // Set the style (with font color) for button
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle(skin.get(TextButton.TextButtonStyle.class));
+        textButtonStyle.font = skin.getFont("descLabelFont");
+        textButtonStyle.fontColor = Color.WHITE;
+        textButtonStyle.downFontColor = Color.valueOf("#e8e288");
+        textButtonStyle.checkedFontColor = Color.YELLOW;
+
+
         // Create UI widgets
         Table table = new Table();
         table.setFillParent(true);
-        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
-        Label.LabelStyle labelStyle = new Label.LabelStyle(skin.getFont("default-font"), skin.getColor("white"));
 
-        Label label = new Label("Sample Text", labelStyle);
-        Label movementLabel = new Label("Movement Control", labelStyle);
+        // Label label = new Label("Sample Text", labelStyle);
+        Label movementLabel = new Label("Movement Control", titleLabelStyle);
+        faceButton = new TextButton("Face", textButtonStyle);
+        touchButton = new TextButton("Touch", textButtonStyle);
+        touchButton.setChecked(true);
+        gyroButton = new TextButton("Gyroscope", textButtonStyle);
 
-        float buttonWidth = 200;
-        faceButton = new TextButton("Face", skin);
-        touchButton = new TextButton("Touch", skin);
-        gyroButton = new TextButton("Gyroscope", skin);
+        Label attackLabel = new Label("Attack Control", titleLabelStyle);
+        tapButton = new TextButton("Tap", textButtonStyle);
+        tapButton.setChecked(true);
+        blinkButton = new TextButton("Eyes", textButtonStyle);
+        voiceButton = new TextButton("Voice", textButtonStyle);
 
-        Label attackLabel = new Label("Attack Control", labelStyle);
-        tapButton = new TextButton("Tap", skin);
-        blinkButton = new TextButton("Eyes", skin);
-        voiceButton = new TextButton("Voice", skin);
 
+        setAllButtonColorToNotSelected();
         setButtonColor();
         // Add listeners to the buttons
         addButtonListener(faceButton, "Use face to control the spaceship", ControlMode.FACE_MODE, null, touchButton, gyroButton);
@@ -68,27 +115,64 @@ public class SettingScreen implements Screen {
         addButtonListener(blinkButton, "Blink eyes to fire laser", null, AttackMode.EYES_BLINKING_MODE, tapButton, voiceButton);
         addButtonListener(voiceButton, "Use voice to fire laser", null, AttackMode.VOICE_MODE, tapButton, blinkButton);
 
-
-        descriptionLabel = new Label("", labelStyle);
+        // Add description
+        descriptionLabel = new Label("", descLabelStyle);
         descriptionLabel.setWrap(true);
         descriptionLabel.setAlignment(Align.center);
 
         // Add widgets to table and stage
-        table.add(movementLabel).colspan(3).padBottom(100).row();
-        table.add(faceButton).width(buttonWidth).padRight(10);
-        table.add(touchButton).width(buttonWidth).padRight(10);
-        table.add(gyroButton).width(buttonWidth).padTop(0).row();
-        table.add(attackLabel).colspan(3).padTop(100).padBottom(100).row();
-        table.add(tapButton).width(buttonWidth).padRight(10);
-        table.add(blinkButton).width(buttonWidth).padRight(10);
-        table.add(voiceButton).width(buttonWidth).padTop(0).row();
+        float buttonWidth = 300;
+        float buttonHeight = 100;
+        table.add(movementLabel).colspan(3).padBottom(100);
+        table.row();
+        table.add(faceButton).width(buttonWidth).height(buttonHeight).padRight(10);
+        table.add(touchButton).width(buttonWidth).height(buttonHeight).padRight(10);
+        table.add(gyroButton).width(buttonWidth).height(buttonHeight).padTop(0);
+        table.row();
+        table.add(attackLabel).colspan(3).padTop(150).padBottom(100);
+        table.row();
+        table.add(tapButton).width(buttonWidth).height(buttonHeight).padRight(10);
+        table.add(blinkButton).width(buttonWidth).height(buttonHeight).padRight(10);
+        table.add(voiceButton).width(buttonWidth).height(buttonHeight).padTop(0);
+        table.row();
         table.row().width(Gdx.graphics.getWidth() - 40).padTop(100); // Add some padding
         table.add(descriptionLabel).colspan(3);
 
-        TextButton backButton = new TextButton("Back", new Skin(Gdx.files.internal("uiskin.json")));
-        backButton.setWidth(200);
-        backButton.setHeight(100);
-        backButton.setPosition((Gdx.graphics.getWidth() - backButton.getWidth()) / 2, 100);
+        stage.addActor(table);
+
+
+
+        // Add back button style - background
+        Texture backButtonTexture_up = new Texture(Gdx.files.internal("buttonBackground.PNG"));
+        Texture backButtonTexture_down = new Texture(Gdx.files.internal("buttonBackground_down.PNG"));
+
+        Drawable backDrawable_up = new TextureRegionDrawable(new TextureRegion(backButtonTexture_up));
+        Drawable backDrawable_down = new TextureRegionDrawable(new TextureRegion(backButtonTexture_down));
+
+        TextButton.TextButtonStyle backStyle = new TextButton.TextButtonStyle();
+        backStyle.up = backDrawable_up; // img when normal
+        backStyle.down = backDrawable_down; // img when clicked
+
+
+        // Add back button style - font
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("font/light_pixel-7.ttf"));
+        parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 50; // set font size
+        BitmapFont buttonFont = generator.generateFont(parameter);
+        generator.dispose();
+        skin.add("buttonFont", buttonFont);
+
+        backStyle.font = skin.getFont("buttonFont");
+        backStyle.fontColor = Color.BLACK;
+        backStyle.downFontColor = Color.GRAY;
+
+
+        // Display back button
+        TextButton backButton = new TextButton("BACK", backStyle);
+
+        backButton.setWidth(BACK_BUTTON_WIDTH);
+        backButton.setHeight(BACK_BUTTON_HEIGHT);
+        backButton.setPosition((Gdx.graphics.getWidth() - backButton.getWidth()) / 2, Gdx.graphics.getHeight() * 0.1f);
 
         backButton.addListener(new ClickListener() {
             @Override
@@ -97,7 +181,6 @@ public class SettingScreen implements Screen {
             }
         });
 
-        stage.addActor(table);
         stage.addActor(backButton);
     }
 
@@ -105,9 +188,11 @@ public class SettingScreen implements Screen {
         activeButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                activeButton.setColor(Color.GREEN);
+                activeButton.setChecked(true);
+//                activeButton.setColor(Color.valueOf(SELECTED_BUTTON_COLOR));
                 for (TextButton button : otherButtons) {
-                    button.setColor(Color.WHITE);
+//                    button.setColor(Color.valueOf(NOT_SELECTED_BUTTON_COLOR));
+                    button.setChecked(false);
                 }
                 descriptionLabel.setText(description);
                 if (controlMode != null) {
@@ -119,21 +204,31 @@ public class SettingScreen implements Screen {
         });
     }
 
+    private void setAllButtonColorToNotSelected() {
+        faceButton.setColor(Color.valueOf(NOT_SELECTED_BUTTON_COLOR));
+        touchButton.setColor(Color.valueOf(NOT_SELECTED_BUTTON_COLOR));
+        gyroButton.setColor(Color.valueOf(NOT_SELECTED_BUTTON_COLOR));
+        tapButton.setColor(Color.valueOf(NOT_SELECTED_BUTTON_COLOR));
+        blinkButton.setColor(Color.valueOf(NOT_SELECTED_BUTTON_COLOR));
+        voiceButton.setColor(Color.valueOf(NOT_SELECTED_BUTTON_COLOR));
+    }
+
     public void setButtonColor() {
+        setAllButtonColorToNotSelected();
         if (SettingScreen.getCurAttackMode() == AttackMode.TAP_MODE) {
-            tapButton.setColor(Color.GREEN);
+            tapButton.setColor(Color.valueOf(SELECTED_BUTTON_COLOR));
         } else if (SettingScreen.getCurAttackMode() == AttackMode.EYES_BLINKING_MODE) {
-            blinkButton.setColor(Color.GREEN);
+            blinkButton.setColor(Color.valueOf(SELECTED_BUTTON_COLOR));
         } else {
-            voiceButton.setColor(Color.GREEN);
+            voiceButton.setColor(Color.valueOf(SELECTED_BUTTON_COLOR));
         }
 
         if (SettingScreen.getCurControlMode() == ControlMode.FACE_MODE) {
-            faceButton.setColor(Color.GREEN);
+            faceButton.setColor(Color.valueOf(SELECTED_BUTTON_COLOR));
         } else if (SettingScreen.getCurControlMode() == ControlMode.TOUCH_MODE) {
-            touchButton.setColor(Color.GREEN);
+            touchButton.setColor(Color.valueOf(SELECTED_BUTTON_COLOR));
         } else {
-            gyroButton.setColor(Color.GREEN);
+            gyroButton.setColor(Color.valueOf(SELECTED_BUTTON_COLOR));
         }
     }
 
